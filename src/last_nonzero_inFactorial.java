03b20903b035566cb8538d06d49ae673ec82f1fe
sriji <srijitasur11b1@gmail.com>
2026-03-24 23:29:10 +0530

    hello

--- src/last_nonzero_inFactorial.java
+++ src/last_nonzero_inFactorial.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class last_nonzero_inFactorial {
+    static int factorial(int n){
+        if(n==1 || n==0)
+            return 1;
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n =sc.nextInt();
+        int fact = factorial(n);
+        System.out.println("factorial of "+ n + " is "+ fact);
+        while(fact%10==0)
+        {
+            fact /= 10;
+        }
+        System.out.println("last non zero digit in factorial of "+ n +" is "+ fact%10);
+    }
 }
